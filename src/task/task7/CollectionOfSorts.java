640a38313cdeadbc61e70163310f25422e35b618
Zidan4ik <roman.pravnyk.tr.2021@lpnu.ua>
2023-12-11 01:33:02 +0200

    add practice task 9.7

--- src/task/task7/CollectionOfSorts.java
+++ src/task/task7/CollectionOfSorts.java
@@ -20,4 +20,49 @@ public class CollectionOfSorts {
         return list;
     }
 
+
+    public static void sort2(){
+    }
+    public static void merge(int[] arr1, int startIndex1, int[] arr2, int startIndex2,
+                             int[] dest, int destStart, int size){  // процедура злиття
+        int index1 = startIndex1;
+        int index2 = startIndex2;
+
+        int endIndex1 = Math.min(startIndex1+size,arr1.length);
+        int endIndex2 = Math.min(startIndex2+size,arr2.length);
+
+        int iteration = (startIndex1-endIndex1)+(startIndex2-endIndex2);
+        for(int i=destStart;i<destStart+iteration;i++){
+            if(index1<endIndex1 && (index2 >= endIndex2 || arr1[index1]<arr2[index2])){
+                // (index2>=endIndex2) обробка ситуацій, коли елементи закінч в одному з масивів
+                // (arr1[index1]<arr2[index2])) порівняння
+                //
+                dest[i]=arr1[index1];
+                index1++;
+            }else{
+                dest[i]=arr2[index2];
+                index2++;
+            }
+        }
+    }
+
+    public static int[] mergeSort(int[] array){
+        int[] tmp; // посередник
+        int[] currentSrc = array; // масив, де зливаємо підмасиви з друг другом
+        int[] currentDest = new int [array.length]; // масив-приймач, де записується масиви, після злиття
+
+        int size = 1; // розмір масива, що буде збільшуватись в (size*2), після злиття
+        while(size<array.length){
+            for(int i=0;i<array.length; i+=2*size){
+                merge(currentSrc,i,currentSrc,i+size,currentDest,i,size);
+            }
+            tmp = currentSrc;
+            currentSrc = currentDest;
+            currentDest = tmp;
+
+            size = size * 2;
+
+        }
+        return currentSrc;
+    }
 }
--- src/task/task7/Starter.java
+++ src/task/task7/Starter.java
@@ -1,5 +1,6 @@
 package task.task7;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static task.task7.CollectionOfSorts.*;
@@ -7,8 +8,13 @@ import static task.task7.CollectionOfSorts.*;
 
 public class Starter {
     public static void main(String[] args) {
-        List<Integer> array = List.of(1,2,111111,5,8,12,6,3,1,123,0,-4,221);
-        List<Integer> array2 = new ArrayList<>(array);
-        System.out.println(sort1(array2));
+//        List<Integer> array = List.of(1,2,111111,5,8,12,6,3,1,123,0,-4,221);
+//        List<Integer> array2 = new ArrayList<>(array);
+//        System.out.println(sort1(array2));
+        int[] arrayInteger = new int[]{8,5,12,27,-7,0,2};
+        mergeSort(arrayInteger);
+        for(int i=0;i<arrayInteger.length;i++){
+            System.out.println(arrayInteger[i]);
+        }
     }
 }
