2a1df20c289761b6274cf6cde7872f6ec4fd7256
Zidan4ik <roman.pravnyk.tr.2021@lpnu.ua>
2023-12-11 20:56:13 +0200

    update practice task 9.7

--- src/task/task7/CollectionOfSorts.java
+++ src/task/task7/CollectionOfSorts.java
@@ -1,19 +1,20 @@
 package task.task7;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CollectionOfSorts {
 
-    public static List<Integer> sort1(List<Integer> list){
+    public static List<Integer> sort1(List<Integer> list) {
 
-        for(int i=0;i<list.size();i++){
-            for(int k=i+1;k<list.size();k++){
+        for (int i = 0; i < list.size(); i++) {
+            for (int k = i + 1; k < list.size(); k++) {
 
-                if(list.get(i)>list.get(k)){
+                if (list.get(i) > list.get(k)) {
                     int value = list.get(i);
-                    list.set(i,list.get(k));
-                    list.set(k,value);
+                    list.set(i, list.get(k));
+                    list.set(k, value);
                 }
             }
         }
@@ -21,48 +22,55 @@ public class CollectionOfSorts {
     }
 
 
-    public static void sort2(){
-    }
-    public static void merge(int[] arr1, int startIndex1, int[] arr2, int startIndex2,
-                             int[] dest, int destStart, int size){  // процедура злиття
-        int index1 = startIndex1;
-        int index2 = startIndex2;
-
-        int endIndex1 = Math.min(startIndex1+size,arr1.length);
-        int endIndex2 = Math.min(startIndex2+size,arr2.length);
-
-        int iteration = (startIndex1-endIndex1)+(startIndex2-endIndex2);
-        for(int i=destStart;i<destStart+iteration;i++){
-            if(index1<endIndex1 && (index2 >= endIndex2 || arr1[index1]<arr2[index2])){
-                // (index2>=endIndex2) обробка ситуацій, коли елементи закінч в одному з масивів
-                // (arr1[index1]<arr2[index2])) порівняння
-                //
-                dest[i]=arr1[index1];
-                index1++;
-            }else{
-                dest[i]=arr2[index2];
-                index2++;
-            }
-        }
+    public static void sort2(int array[]) {
+        mergeSort(array);
     }
 
-    public static int[] mergeSort(int[] array){
-        int[] tmp; // посередник
-        int[] currentSrc = array; // масив, де зливаємо підмасиви з друг другом
-        int[] currentDest = new int [array.length]; // масив-приймач, де записується масиви, після злиття
+    public static void mergeSort(int array[]) {//
+        int arr = array.length; //
+        if(arr == 1) return;
+        int mid = arr / 2;
 
-        int size = 1; // розмір масива, що буде збільшуватись в (size*2), після злиття
-        while(size<array.length){
-            for(int i=0;i<array.length; i+=2*size){
-                merge(currentSrc,i,currentSrc,i+size,currentDest,i,size);
-            }
-            tmp = currentSrc;
-            currentSrc = currentDest;
-            currentDest = tmp;
+        int[] part1 = new int[mid];
+        int[] part2 = new int[arr - mid];
 
-            size = size * 2;
-
+        for (int i = 0; i < mid; i++) {  // copy elements
+            part1[i] = array[i];
         }
-        return currentSrc;
+        for (int i = mid; i < arr; i++) { // copy elements
+            part2[i-mid] = array[i];
+        }
+        mergeSort(part1);
+        mergeSort(part2);
+        merge(array, part1, part2);
+
+    }
+
+    private static void merge(int arr[], int[] part1, int[] part2) {
+        int left = part1.length;
+        int right = part2.length;
+
+        int i1 = 0;
+        int i2 = 0;
+        int idx = 0;
+// loop for comparing elements
+        while (i1 < left && i2 < right) {
+            if (part1[i1] < part2[i2]) {
+                arr[idx] = part1[i1];
+                i1++;
+            } else {
+                arr[idx] = part2[i2];
+                i2++;
+            }
+            idx++;
+        }
+// next two loop recopy another elements for saving structure array
+        for (int s1 = i1; s1 < left; s1++) {
+            arr[idx++] = part1[s1];
+        }
+        for (int s2 = i2; s2 < right; s2++) {
+            arr[idx++] = part2[s2];
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
--- src/task/task7/Starter.java
+++ src/task/task7/Starter.java
@@ -8,13 +8,19 @@ import static task.task7.CollectionOfSorts.*;
 
 public class Starter {
     public static void main(String[] args) {
-//        List<Integer> array = List.of(1,2,111111,5,8,12,6,3,1,123,0,-4,221);
-//        List<Integer> array2 = new ArrayList<>(array);
-//        System.out.println(sort1(array2));
-        int[] arrayInteger = new int[]{8,5,12,27,-7,0,2};
-        mergeSort(arrayInteger);
+        List<Integer> array = List.of(1,2,111111,5,8,12,6,3,1,123,0,-4,221);
+        List<Integer> array2 = new ArrayList<>(array);
+        System.out.println(sort1(array2));
+
+
+
+        int[] arrayInteger = {8,5,12,27,0,2,6,-7,0,3,4,6};
+        System.out.println(Arrays.toString(arrayInteger));
+        sort2(arrayInteger);
+        sort2(new int[]{1,2,111111,5,8,12,6,3,1,123,0,-4,221});
+
         for(int i=0;i<arrayInteger.length;i++){
-            System.out.println(arrayInteger[i]);
+            System.out.print(arrayInteger[i] + " ");
         }
     }
 }
