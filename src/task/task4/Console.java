c2b6ba0820041b5995a5bd9a886d03fe8e6246bf
Zidan4ik <roman.pravnyk.tr.2021@lpnu.ua>
2023-12-13 20:35:04 +0200

    update practice task 9.4

--- src/task/task4/Console.java
+++ src/task/task4/Console.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class Console {
     public void doConsole() {
-        Map<String,String> gos_number = new HashMap();
+        Map<String, String> gos_number = new HashMap();
         Scanner scanner = new Scanner(System.in);
 
         String num = "";
@@ -13,28 +13,29 @@ public class Console {
         String key = "";
         while (!key.equals("stop")) {
             if (key.equals("list")) {
-                for (Map.Entry<String,String> l : gos_number.entrySet()) {
-                    System.out.println("Власник: "+l.getValue()+" | номер автомобіля: "+l.getKey());
+                for (Map.Entry<String, String> l : gos_number.entrySet()) {
+                    System.out.println("Власник: " + l.getValue() + " | номер автомобіля: " + l.getKey());
                 }
-            }
-            else if(key.equals("cars")){
-                for (Map.Entry<String,String> l : gos_number.entrySet()) {
-                    System.out.println("Номер автомобіля: "+l.getKey());
+            } else if (key.equals("cars")) {
+                for (Map.Entry<String, String> l : gos_number.entrySet()) {
+                    System.out.println("Номер автомобіля: " + l.getKey());
                 }
-            }
-            else if(key.equals("own")){
-                for (Map.Entry<String,String> l : gos_number.entrySet()) {
-                    System.out.println("Власник: "+l.getValue());
+            } else if (key.equals("own")) {
+                Set<String> personalName = new HashSet<>();
+                for (Map.Entry<String, String> l : gos_number.entrySet()) {
+                    personalName.add(l.getValue());
                 }
-            }
-            else if(key.equals("add")) {
+                for(String n: personalName){
+                    System.out.println("Власник: " + n);
+                }
+            } else if (key.equals("add")) {
                 System.out.println("Введіть номер для авто: ");
                 num = scanner.nextLine();
                 System.out.println("Введіть власника автомобіля: ");
                 name = scanner.nextLine();
 
                 if (num.length() == 8) {
-                    gos_number.put(num,name);
+                    gos_number.put(num, name);
                 } else {
                     System.out.println("ваш номер не має входить в номерні знаки автомобілів України");
                 }
